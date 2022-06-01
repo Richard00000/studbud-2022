21196bd8bbf0198e14091b8a7a016aed01b23af1
Richard00000 <53285788+Richard00000@users.noreply.github.com>
2022-06-01 20:28:32 +0800

    20220601 More annotate

--- public/pomodoro.js
+++ public/pomodoro.js
@@ -1,4 +1,5 @@
 var pomodoro = {
+    // seting the variable and the state for the timer
     started: false,
     minutes: 0,
     seconds: 0,
@@ -6,13 +7,14 @@ var pomodoro = {
     minDom: null,
     secDom: null,
     init: function () {
+        // Basic form DOM elements
         var self = this;
         this.minDom = document.querySelector('#minutes');
         this.secDom = document.querySelector('#seconds');
         this.interval = setInterval(function () {
             self.intervalCallback.apply(self);
         }, 1000);
-
+        //listener for the button of the pomodoro and start the function
         document.querySelector('#b_start').onclick = function () {
             self.startWork.apply(self);
         };
@@ -26,7 +28,7 @@ var pomodoro = {
             self.stopTimer.apply(self);
         };
     },
-
+    //function for reset the variables
     startWork: function () {
         this.resetVariables(25, 0, true);
     },
@@ -40,24 +42,25 @@ var pomodoro = {
         this.resetVariables(25, 0, false);
         this.updateDom();
     },
+    //function to set the number that less than 2 digit to 2 digit
     toDoubleDigit: function (num) {
         if (num < 10) {
             return "0" + parseInt(num, 10);
         }
         return num;
     },
-
+    // Function to set the time as parameters
     resetVariables: function (mins, secs, started) {
         this.minutes = mins;
         this.seconds = secs;
         this.started = started;
     },
-
+    // Function to update the Dom
     updateDom: function () {
         this.minDom.innerHTML = this.toDoubleDigit(this.minutes);
         this.secDom.innerHTML = this.toDoubleDigit(this.seconds);
     },
-
+    // The main logic of the pomodoro
     intervalCallback: function () {
         if (!this.started) return false;
         if (this.seconds == 0) {
@@ -76,6 +79,7 @@ var pomodoro = {
         this.started = false;
     }
 };
+//load the function immediately when the page loaded
 window.onload = function () {
     pomodoro.init();
 };
